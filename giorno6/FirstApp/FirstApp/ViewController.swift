42e7fdbb096ae26e5a223f61f8034975c5278468
hiroexe <stefano.carella93@gmail.com>
2022-06-14 10:47:22 +0200

    day7

--- giorno6/FirstApp/FirstApp/ViewController.swift
+++ giorno6/FirstApp/FirstApp/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBAction func calcolaButton(_ sender: UIButton) {
+        print("fuuuuuuck")
+        if helloLabel.text == "Buongiorno" {
+            helloLabel.text = "Buongiorno un cazzo"
+            helloLabel.textColor = UIColor.systemPink
+        } else {
+            helloLabel.text = "Buongiorno"
+            helloLabel.textColor = UIColor.gray
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hellooooo world")
     }
 
 
