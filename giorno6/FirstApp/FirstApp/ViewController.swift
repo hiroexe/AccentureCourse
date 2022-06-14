5d370de0a172a8b00e40db68957b3667a5efcf47
hiroexe <stefano.carella93@gmail.com>
2022-06-14 09:40:27 +0200

    day7

---
+++ giorno6/FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Stefano carella on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hellooooo world")
+    }
+
+
+}
+
